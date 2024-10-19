4465a212a1ea9ce1bb257eeac3befcda9f6f30c9
Aeek True <aeektrue@gmail.com>
2024-10-19 17:53:57 +0300

    value adapters added

--- dpg_forms/__init__.py
+++ dpg_forms/__init__.py
@@ -16,12 +16,18 @@ class BaseForm:
     Model: type
     field_types: dict
     submit_callback: Callable
-
-    def __init_subclass__(cls, model, callback, type_hints_patch=None) -> None:
+    value_adapters: dict
+    def __init_subclass__(cls, model, callback,
+        type_hints_patch=None,
+        value_adapters=None) -> None:
         cls.Model = model
         cls.field_types = get_type_hints(model)
         if type_hints_patch is not None:
             cls.field_types.update(type_hints_patch)
+
+        cls.value_adapters = dict()
+        if value_adapters is not None:
+            cls.value_adapters.update(value_adapters)
         cls.submit_callback = callback
 
     def __init__(self, label=''):
@@ -41,9 +47,16 @@ class BaseForm:
                 self.submit_button = dpg.add_button(label='Submit', callback=self.submit_form)
 
     def submit_form(self, sender, app_data, user_data):
-        raw_values = {n: v for n, v in zip(self.field_names, dpg.get_values(self.field_ids))}
+        raw_values = {n: self.adapt_value(n, v) for n, v in zip(self.field_names, dpg.get_values(self.field_ids))}
         o = self.Model(**raw_values)
         self.submit_callback(o)
+
+    def adapt_value(self, field_name, value):
+        adapter = self.value_adapters.get(field_name)
+        if adapter is not None:
+            return adapter(value)
+        else:
+            return value
 
     def add_me(self):
         dpg.unstage(self._staging_containter_id)
